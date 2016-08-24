6a76c9cd35193e25fc861a759d2e9b149d5a1d02
Volorf <frolololeg@gmail.com>
2016-08-24 22:02:01 +0300

    Added demo data to textField

--- sparkliner.sketchplugin/Contents/Sketch/sparkliner.js
+++ sparkliner.sketchplugin/Contents/Sketch/sparkliner.js
@@ -110,7 +110,7 @@ function enterData(context) {
 	var selectedCount = selectedLayers.count();
 	var doc = context.document;
 
-	var dataFromTextFieldArray = doc.askForUserInput_initialValue("Enter your data", "")
+	var dataFromTextFieldArray = doc.askForUserInput_initialValue("Enter your data", "10, 30, 3, 8, 39, 25, 22, 89, 74, 7, 2, 40, 61, 17, 35, 31, 8, 14, 39, 32, 53")
 
 	var dataArray = dataFromTextFieldArray.trim().split(", ").map(Number)
 
