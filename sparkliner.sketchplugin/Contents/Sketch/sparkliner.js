193757f6c7fa2deb146a3928dd29c9f9a8b46310
Oleg Frolov <frolololeg@gmail.com>
2017-02-16 18:43:06 +0300

    Added a random data function

--- sparkliner.sketchplugin/Contents/Sketch/sparkliner.js
+++ sparkliner.sketchplugin/Contents/Sketch/sparkliner.js
@@ -135,15 +135,11 @@ function createWithJson(context) {
  * @param  {Object} context
  */
 function enterData(context) {
-
   var doc = context.document;
-
 	var dataFromTextFieldArray = doc.askForUserInput_initialValue("Enter your data", "10, 30, 3, 8, 39, 25, 22, 89, 74, 7, 2, 40, 61, 17, 35, 31, 8, 14, 39, 32, 53");
-
 	var dataArray = dataFromTextFieldArray.trim().split(", ").map(Number);
-
+  defaults.displayEndPoint = false
   var options = Object.assign({}, defaults, { dataArray: dataArray });
-
   generateSparkline(context, options);
 }
 
@@ -153,13 +149,14 @@ function enterData(context) {
  * @param  {Object} context
  */
 function randomData(context) {
-
+  var doc = context.document;
+  var dataFromTextFieldArray = doc.askForUserInput_initialValue("Enter amount of items (more two)", "23");
   var dataArray = [];
-
-  for (var i = 0; i < 20; i++) dataArray.push(Math.floor(Math.random() * 100));
-
+  for (var i = 0; i < dataFromTextFieldArray; i++) {
+    dataArray.push(Math.floor(Math.random() * 100));
+  }
+  defaults.displayEndPoint = false
   var options = Object.assign({}, defaults, { dataArray: dataArray });
-
   generateSparkline(context, options);
 
 }
