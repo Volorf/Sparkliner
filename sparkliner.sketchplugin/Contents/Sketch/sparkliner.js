55152496840958906b9baf36510faf97c300fc94
Volorf <frolololeg@gmail.com>
2017-02-17 08:58:25 +0300

    Fixed bugs

--- sparkliner.sketchplugin/Contents/Sketch/sparkliner.js
+++ sparkliner.sketchplugin/Contents/Sketch/sparkliner.js
@@ -150,10 +150,13 @@ function enterData(context) {
  */
 function randomData(context) {
   var doc = context.document;
+  // var selectedLayers = context.selection;
+  // var box = selectedLayers[0];
+  // var boxHeight = box.frame().height();
   var dataFromTextFieldArray = doc.askForUserInput_initialValue("Enter amount of items (more two)", "23");
   var dataArray = [];
   for (var i = 0; i < dataFromTextFieldArray; i++) {
-    dataArray.push(Math.floor(Math.random() * 100));
+    dataArray.push(Math.floor(Math.random() * 1000));
   }
   defaults.displayEndPoint = false
   var options = Object.assign({}, defaults, { dataArray: dataArray });
